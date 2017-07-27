4a33900ebed8e452b1145d84a80f2c83f19f0ce2
Chris Givanoudis <chgivan@gmail.com>
2017-07-27 17:10:07 +0300

    Added metadataService

--- doc.md
+++ doc.md
@@ -31,12 +31,12 @@ API
 #POST /articles ;; Create a new article, user must be login
 #PUT /articles ;; Update a new article, user must be login
 GET /search ;; Returns a list of articles base on the search query
-GET /articles/<articleID>/comments ;;Returns a list of comments of  the article, must be login
-POST /articles/<articleID>/comments ;;Create a new comment of the article
+#GET /articles/<articleID>/comments ;;Returns a list of comments of  the article, must be login
+#POST /articles/<articleID>/comments ;;Create a new comment of the article
 # GET /users/<userID> ;;Return the userName with userID, must be login and owner of the userID
 POST /login ;;Login a user gets username and password
 # POST /users ;;Register a new user gets username and password, username must not be exist
 # PUT /user/<userID> ;; change the password of the user, must be login and owner of the userID
-POST /articles/<articleID>/like ;;increase the like count user must be login
-POST /articles/<articleID>/dislike ;;increase the dislike count user must be login
-GET /articles/<articleID>/metadata;; return the likes of the articlef
+#POST /articles/<articleID>/like ;;increase the like count user must be login
+#POST /articles/<articleID>/dislike ;;increase the dislike count user must be login
+#GET /articles/<articleID>/metadata;; return the likes of the articlef
---
+++ metadataService/metadataService.py
@@ -0,0 +1,77 @@
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+cache = {
+    "0":{
+        "title":"Hello World",
+        "likes":123,
+        "dislikes":32,
+        "views":89322,
+        "authorID":"0",
+        "authorUsername":"Chris"
+    }
+}
+
+@app.route("/articles/<articleID>/like", methods=["GET"])
+def like(articleID):
+    global cache
+    if not articleID in cache:
+        return getResponse(404,"Article with id {} doesn't exist".format(articleID))
+    cache[articleID]["likes"] += 1
+    return getResponse(200)
+
+@app.route("/articles/<articleID>/dislike", methods=["GET"])
+def dislike(articleID):
+    global cache
+    if not articleID in cache:
+        return getResponse(404,"Article with id {} doesn't exist".format(articleID))
+    cache[articleID]["dislikes"] += 1
+    return getResponse(200)
+
+@app.route("/articles/<articleID>/metadata", methods=["GET"])
+def getArticleMetadata(articleID):
+    if not articleID in cache:
+        return getResponse(404,"Article with id {} doesn't exist".format(articleID))
+    metadata = cache[articleID]
+    return getResponse(
+        200,
+        title=metadata["title"],
+        likes=metadata["likes"],
+        dislikes=metadata["dislikes"],
+        views=metadata["views"],
+        get_author="/users/{}".format(metadata["authorID"]),
+        post_like="/articles/{}/like".format(articleID),
+        post_dislike="/articles/{}/dislike".format(articleID),
+        authorUsername=metadata["authorUsername"]
+    )
+
+@app.route("/articles", methods=["GET"])
+def listArticlesMetadata():
+    articleList = []
+    for articleID, metadata in cache.items():
+        articleList.append({
+            "title":metadata["title"],
+            "views":metadata["views"],
+            "authorUsername":metadata["authorUsername"],
+            "get_author":"/users/{}".format(metadata["authorID"]),
+            "get_article":"/articles/{}".format(articleID)
+        })
+    return getResponseList(200, articleList)
+
+def getResponseList(status, aList):
+    if aList is None:
+        aList = []
+    resp = jsonify(aList)
+    resp.status_code = status
+    return resp
+    
+def getResponse(status, **kwargs):
+    obj = {}
+    if kwargs is not None:
+        obj = kwargs
+    resp = jsonify(obj)
+    resp.status_code = status
+    return resp    
+
