591762a2b89eb83a22e2ee00fdd9d73191f74649
Chris Givanoudis <chgivan@gmail.com>
2017-07-29 18:59:19 +0300

    Added intercommunication with rabbitmq

--- articlesBackEnd/articleBackEnd.py
+++ articlesBackEnd/articleBackEnd.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from uuid import uuid4
-import pika
+import pika, json
 
 app = Flask(__name__)
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100'))
@@ -20,23 +20,29 @@ def getArticle(articleID):
    return articles[articleID]
 
 @app.route("/articles", methods=["POST"])
-
-def createArticle():
-   if request.data is None:
-      return getResponse(400, message=errMsg)
-
+def createAsrticle():
    global articles
    id = str(uuid4())
-   articles[id] = request.data
+   data = request.get_json()
+   articles[id] = data["body"]
+   sendObj = {
+      "articleID":id,
+      "title":data["title"],
+      "authorID":"0"
+   }
 
    channel.basic_publish(
       exchange='',
-      routing_key='hello',
-      body='Hello World'
+      routing_key='createArticle',
+      body=json.dumps(sendObj)
    )
    
    
-   return getResponse(201,get="/articles/{}".format(id))
+   return getResponse(
+      201,
+      get="/articles/{}".format(id),
+      metadata="/articles/{}/metadata".format(id)
+   )
 
 @app.route("/articles/<articleID>", methods=["PUT"])
 def updateArticle(articleID):
@@ -60,3 +66,6 @@ def getResponse(status, **kwargs):
     resp = jsonify(obj)
     resp.status_code = status
     return resp
+
+if __name__=='__main__':
+   app.run(debug=True, port=5000)
--- metadataService/metadataService.py
+++ metadataService/metadataService.py
@@ -1,11 +1,17 @@
-from threading import Thread
+import threading, json
+from time import sleep
 from flask import Flask, jsonify, request
-import pika
+import pika, redis
 
+from diskcache import Cache
+
+#globals
+redisDB = redis.StrictRedis(host='192.168.99.100', port=6379, db=0)
 app = Flask(__name__)
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100'))
 channel = connection.channel()
 channel.queue_declare(queue="createArticle")
+internal_lock = threading.Lock()
 
 cache = {
     "0":{
@@ -18,17 +24,29 @@ cache = {
     }
 }
 
-def OnArticleCreated():
-    print("Received Message")
-    
-def testThread():
-    channel.basic_consume(OnArticleCreated,queue="createArticle",no_ack=True)
-    channel.start_consuming()
-    print("Start Consuming")
-    
-thread = Thread(target=testThread)
-thread.start()
+#Message Bus communications
+def OnArticleCreated(ch, method, prop, body):
+    print(str(json.loads(body)))
+    obj = json.loads(body)
+    id = obj['articleID']
+    with redisDB.pipeline() as pipe:
+        pipe.hset(id, "title", obj['title'])
+        pipe.hset(id, "authorUsername", "Change this later")
+        pipe.hset(id, "authorID", obj["authorID"])
+        pipe.hset(id, "views", 0)
+        pipe.hset(id, "likes", 0)
+        pipe.hset(id, "dislikes", 0)
+        r = pipe.execute()
+        print(r)
+                  
+def _process_data_events():
+    channel.basic_consume(OnArticleCreated, queue='createArticle', no_ack=True)
+    while True:
+        with internal_lock:
+            connection.process_data_events()
+            sleep(0.1)
 
+#Rest EndPoints
 @app.route("/articles/<articleID>/like", methods=["GET"])
 def like(articleID):
     global cache
@@ -47,20 +65,27 @@ def dislike(articleID):
 
 @app.route("/articles/<articleID>/metadata", methods=["GET"])
 def getArticleMetadata(articleID):
-    if not articleID in cache:
-        return getResponse(404,"Article with id {} doesn't exist".format(articleID))
-    metadata = cache[articleID]
+    if not redisDB.exists(articleID):
+        return getResponse(404,message="Article with id {} doesn't exist".format(articleID))
+    pipe = redisDB.pipeline()
+    pipe.hget(articleID, "title")
+    pipe.hget(articleID, "views")
+    pipe.hget(articleID, "likes")
+    pipe.hget(articleID, "dislikes")
+    pipe.hget(articleID, "authorID")
+    pipe.hget(articleID, "authorUsername")
+    rList = pipe.execute()
+    print(rList)
     return getResponse(
         200,
-        title=metadata["title"],
-        likes=metadata["likes"],
-        dislikes=metadata["dislikes"],
-        views=metadata["views"],
-        get_author="/users/{}".format(metadata["authorID"]),
-        post_like="/articles/{}/like".format(articleID),
-        post_dislike="/articles/{}/dislike".format(articleID),
-        authorUsername=metadata["authorUsername"]
-    )
+        title=rList[0].decode("utf-8"),
+        views=int(rList[1]),
+        likes=int(rList[2]),
+        dislikes=int(rList[3]),
+        get_author="/users/{}".format(rList[4].decode("utf-8")),
+        get="/articles/{}".format(articleID),
+        authorUsername=rList[5].decode("utf-8")
+    )  
 
 @app.route("/articles", methods=["GET"])
 def listArticlesMetadata():
@@ -75,6 +100,7 @@ def listArticlesMetadata():
         })
     return getResponseList(200, articleList)
 
+#Utils Functions
 def getResponseList(status, aList):
     if aList is None:
         aList = []
@@ -90,3 +116,9 @@ def getResponse(status, **kwargs):
     resp.status_code = status
     return resp    
 
+#Running Program
+thread = threading.Thread(target=_process_data_events)
+thread.setDaemon(True)
+if __name__=='__main__':
+    thread.start()
+    app.run(debug=True, port=5001)
