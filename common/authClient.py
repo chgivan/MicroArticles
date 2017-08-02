e0a381cc4dd1e0455073237113f5542218639903
Chris Givanoudis <chgivan@gmail.com>
2017-08-02 18:35:45 +0300

    Added Auth

--- common/authClient.py
+++ common/authClient.py
@@ -1,39 +1,45 @@
-import pika, uuid
+import pika
+import threading
+from time import sleep 
+import json
 
 class AuthClient(object):
-    def __init__ (self, host="localhost", port=5672):
-        self.connection = pika.BlockingConnection(
-            host = host,
-            port = port
+    internal_lock = threading.Lock()
+    tokens = {}
+    def __init__ (self, host="localhost"):
+        self.connection= pika.BlockingConnection(
+            pika.ConnectionParameters(host=host)
         )
         self.channel = self.connection.channel()
-        result = self.channel_queue_declare(exclusive=True)
-        self.callback_queue = result.method.queue
-        self.channel.basic_consume(
-            self.on_response,
-            no_ack=True,
-            queue=self.callback_queue
-        )
+        self.channel.exchange_declare(exchange='auth', type='fanout')
+        result = self.channel.queue_declare(exclusive=True)
+        self.queue_name = result.method.queue
+        self.channel.queue_bind(exchange = 'auth', queue = self.queue_name)
+        thread = threading.Thread(target=self._process_data_events)
+        thread.setDaemon(True)
+        thread.start()
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
+    def _process_data_events(self):
+        self.channel.basic_consume(self._on_response, no_ack=True, queue=self.queue_name)
+        while True:
+            with self.internal_lock:
+                self.connection.process_data_events()
+                sleep(0.1)
 
-    def isValidToken_Remote(self, token):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange="",
-            routing_key='rpc_validToken',
-            properties=pika.BasicProperties(
-                reply_to = self.callback_queue,
-                correlation_id = self.corr_id,
-            ),
-            body = token
-        )
-        while self.response is None:
-            self.connection.process_data_events()
-        return bool(self.response)
+    def _on_response(self, ch, method, props, body):
+        data = json.loads(body)
+        if data is not None:
+            self.tokens[data["token"]] = data["id"]
+        print(self.tokens)
+
+    def isTokenValid(self, token, userID):
+        if not token in self.tokens:
+            return False
+        return int(self.tokens[token]) == int(userID)
+
+    def close():
+        self.connection.close()
 
 authClient = AuthClient(host="192.168.99.100")
-print(authClient.isValidToken_Remote("355325353gs3532"))
+while True:
+    sleep(0.1)
--- users/usersBackend.py
+++ users/usersBackend.py
@@ -1,15 +1,18 @@
 #Users BackEnd
 from pony import orm
 from flask import Flask, jsonify, request
+import pika
+import uuid, json
+
 app = Flask(__name__)
-
-users = {
-    "0":{"username":"testtestopoulos", "password":"2323g4ffeddf"},
-    "1":{"username":"efkowekgfs", "password":"433424334"}
-}
-usersCount = 1
 db = orm.Database("sqlite", "tmp.sqlite", create_db=True)
+connMQ = pika.BlockingConnection(
+    pika.ConnectionParameters(host="192.168.99.100")
+)
+channelMQ = connMQ.channel()
+channelMQ.exchange_declare(exchange='auth', type='fanout')
 
+tokens = {}
 class User(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     username = orm.Required(str, unique=True)
@@ -18,8 +21,39 @@ class User(db.Entity):
 db.generate_mapping(create_tables=True)
 orm.sql_debug(True)
 
+@app.route("/login", methods=['POST'])
+def login():
+    params = request.json
+    errFlag, errMsg = validParams(params)
+    if errFlag:
+        return getResponse(400, message=errMsg)
+
+    try:
+        with orm.db_session:
+            user = orm.select(
+                user for user in User
+                if user.password == params["password"]
+                and user.username == params["username"]
+            ).first()
+            global tokens
+            token = str(uuid.uuid4())
+            tokens[token] = user.id
+            channelMQ.basic_publish(
+                exchange='auth',
+                routing_key='',
+                body=json.dumps({"id":user.id,"token":token})
+            )
+            return getResponse(200, userID = user.id, token=token)
+    except orm.core.ObjectNotFound:
+        return getResponse(404, message="Wrong username or password")
+
 @app.route("/users/<userID>", methods=["GET"])
 def getUser(userID):
+    token = request.args.get('token')
+    if token is None:
+        return getResponse(400,message="Missing token!")
+    if not validToken(token=token, userID=userID):
+        return getResponse(403,message="Access Denied!!")
     try:
         with orm.db_session():
             user = User[userID]
@@ -34,17 +68,10 @@ def getUser(userID):
 
 @app.route("/users", methods=["POST"])
 def createUser():
-    errorFlag = False
-    errorMsg = ""
     params = request.json
-    if not "username" in params:
-        errorMsg += "Missing username field.\n"
-        errorFlag = True
-    if not "password" in params:
-        errorMsg += "Missing password field.\n"
-        errorFlag = True
-    if errorFlag:
-        return getResponse(400, message=error)
+    errFlag, errMsg = validParams(params)
+    if errFlag:
+        return getResponse(400, message=errMsg)
 
     userID = None
     try:
@@ -66,6 +93,11 @@ def createUser():
 @app.route("/users/<userID>", methods=["PUT"])
 def updateUser(userID):
     params = request.json
+    if not "token" in params:
+        return getResponse(400,message="Missing token!")
+    if not validToken(token=params["token"],userID = userID):
+        return getResponse(403,message="Access Denied!!")
+
     if not "password" in params:
         return getResponse(400, message="No password given!!") 
 
@@ -102,6 +134,23 @@ def listUsers():
     resp = jsonify(results)
     resp.status_code = 200
     return resp
+
+def validToken(token, userID):
+    global tokens
+    if not token in tokens:
+        return False
+    return int(tokens[token]) == int(userID)
+
+def validParams(params):
+    errorFlag = False
+    errorMsg = ""
+    if not "username" in params:
+        errorMsg += "Missing username field.\n"
+        errorFlag = True
+    if not "password" in params:
+        errorMsg += "Missing password field.\n"
+        errorFlag = True
+    return errorFlag, errorMsg
 
 def getResponse(status, **kwargs):
     obj = {}
